8dd54f975183868dd41969456d146dc599648fe9
sunghyukpark <sunghyukpark0408@gmail.com>
2017-10-02 23:19:19 -0500

    Implemented get_next_state

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+from state import State
+import sys
 
+def main():
+    state = State([[1,2,0],[0,0,3],[0,0,0]])
+    #state2 = State([[1,2,0],[0,0,3],[0,0,0]])
+    state3 = State([[1,2,0],[0,0,3],[0,0,0]])
+    state4 = State([[1,2,0],[0,0,3],[0,0,0]])
+    #state5 = State([[0,0,0],[0,0,0],[0,0,0]])
+    state6 = State([[0,0,0],[0,0,0],[0,0,0]])
+
+    state1 = State([[0,0,0],[0,0,0],[0,0,0]])
+
+    new_state = state.get_next_state(1,2)
+    #new_state2 = state2.get_next_state(1,0)
+    new_state3 = state3.get_next_state(0,2)
+    new_state4 = state4.get_next_state(0,1)
+    #new_state5 = state5.get_next_state(1,0)
+    #new_state6 = state6.get_next_state(1,2)
+    print("state", state.towers)
+    print new_state.towers
+    #print("state", state.towers)
+    #print new_state2.towers
+    print("state", state.towers)
+    print new_state3.towers
+    print("state", state.towers)
+    print new_state4.towers
+    #print("state", state1.towers)
+    #print new_state5.towers
+    #print("state", state1.towers)
+    #print new_state6.towers
+
+
+main()
 
 
--- state.py
+++ state.py
@@ -1,7 +1,51 @@
+import sys
+import copy
+
 class State:
 
-    def __init__(self, tower_1, tower_2, tower_3):
-        self.tower_1 = tower_1
-        self.tower_2 = tower_2
-        self.tower_3 = tower_3
+    def __init__(self, towers):
+        # towers = 2d array of size 3
+        # [[],[],[]]
+        self.towers = towers
 
+    def can_move(self, from_index, to_index):
+        from_tower = self.towers[from_index]
+        to_tower = self.towers[to_index]
+
+        if self.is_empty(from_tower) is True:
+            return False
+            
+        if self.top(from_tower) < self.top(to_tower) or self.is_empty(to_tower):
+            return True
+
+        return False
+
+    def is_empty(self, tower):
+        return all(v == 0 for v in tower)
+
+    def top(self, tower):
+        for disk in tower:
+            if disk != 0:
+                return disk
+        return 0 
+        # returns none if next state is not available
+    def get_next_state(self, from_index, to_index):
+
+        if self.can_move(from_index, to_index) == False:
+            return
+
+        next_state = copy.deepcopy(State(self.towers))
+
+        from_tower = next_state.towers[from_index]
+        to_tower = next_state.towers[to_index]
+
+        top_from_index = self.top(from_tower)-1
+        top_to_index = self.top(to_tower)-1
+
+        # set to_tower value
+        next_state.towers[to_index][top_from_index] = self.top(from_tower) 
+
+        # set from_tower value
+        next_state.towers[from_index][top_from_index] = 0
+
+        return next_state    
